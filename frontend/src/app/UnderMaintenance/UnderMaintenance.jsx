9a94a8cdbf2b34783a2ad39386e6452d316cbe7a
Paul Thanataweenont <paul@chronos.email>
2023-07-31 23:42:15 -0600

    update content

--- frontend/src/app/UnderMaintenance/UnderMaintenance.jsx
+++ frontend/src/app/UnderMaintenance/UnderMaintenance.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function UnderMaintenance() {
   return (
-    <div className="container">
-      <h1>Website Under Maintenance</h1>
+    <div className="container" style={{textAlign:"center"}}>
+      <h1>Cella-Mi Store</h1>
+      <h2>Website Under Maintenance</h2>
+      <h3>Comming Soon</h3>
     </div>
   );
 }
